e735b00320a2c1ec17c252790448f584bbacb730
Jiawen Geng <technicalcute@gmail.com>
2019-09-07 13:46:44 +0800

    Add e2e test (#27)
    * Add e2e test
    
    * Add e2e test
    
    * Add e2e test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 import path from 'path'
 import test from 'ava'
 import sao from 'sao'
+import execa from 'execa'
 
 const generator = path.join(__dirname, '..')
 
@@ -8,4 +9,26 @@ test('defaults', async t => {
   const stream = await sao.mock({ generator })
 
   t.snapshot(stream.fileList, 'Generated files')
+})
+
+const log = i => {
+  console.log(i.stdout)
+  console.log(i.stderr)
+}
+
+test('e2e', async t => {
+  const outDir = path.join(__dirname, '..', 'sample_out')
+  await execa.command(`npx sao ${generator} ${outDir} -y`, {
+    cwd: path.join(__dirname, '..'),
+  })
+    .then(log)
+  await execa.command(`yarn`, {
+    cwd: outDir,
+  })
+    .then(log)
+  await execa.command(`yarn test`, {
+    cwd: outDir,
+  })
+    .then(log)
+  t.pass('template e2e works')
 })
