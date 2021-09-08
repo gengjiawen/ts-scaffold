93909e9610baf5b768a8ba1a9b2165acb111e350
Jiawen Geng <technicalcute@gmail.com>
2021-09-08 08:02:13 +0000

    chore: format files

--- saofile.js
+++ saofile.js
@@ -1,5 +1,5 @@
 const superb = require('superb')
-const {execSync} = require('child_process')
+const { execSync } = require('child_process')
 
 module.exports = {
   prompts() {
@@ -8,50 +8,50 @@ module.exports = {
         name: 'name',
         message: 'What is the name of the new project',
         default: this.outFolder,
-        filter: val => val.toLowerCase()
+        filter: (val) => val.toLowerCase(),
       },
       {
         name: 'description',
         message: 'How would you describe the new project',
-        default: `my ${superb.random()} project`
+        default: `my ${superb.random()} project`,
       },
       {
         name: 'username',
         message: 'What is your GitHub username',
         default: this.gitUser.username || this.gitUser.name,
-        filter: val => val.toLowerCase(),
-        store: true
+        filter: (val) => val.toLowerCase(),
+        store: true,
       },
       {
         name: 'email',
         message: 'What is your email?',
         default: this.gitUser.email,
-        store: true
+        store: true,
       },
     ]
   },
   actions: [
     {
       type: 'add',
-      files: '**'
+      files: '**',
     },
     {
       type: 'move',
       patterns: {
-        gitignore: '.gitignore'
-      }
-    }
+        gitignore: '.gitignore',
+      },
+    },
   ],
   async completed() {
     this.gitInit()
     execSync('npx husky-init', {
-      cwd: this.outFolder
+      cwd: this.outFolder,
     })
     execSync('npx husky add .husky/pre-commit "npx lint-staged"', {
-      cwd: this.outFolder
+      cwd: this.outFolder,
     })
 
     this.npmInstall()
     this.showProjectTips()
-  }
+  },
 }
