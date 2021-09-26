60ad1d66347a6d0c436b6299d4659eb74310e6d4
Jiawen Geng <technicalcute@gmail.com>
2021-09-26 06:13:43 +0000

    feat: add koa sample

---
+++ extensions/koa/index.ts
@@ -0,0 +1,22 @@
+import * as Koa from 'koa'
+import * as Router from '@koa/router'
+import * as cors from '@koa/cors'
+import * as koaBody from 'koa-body'
+
+const app = new Koa()
+
+app.use(require('koa-compress')())
+app.use(koaBody())
+const router = new Router()
+
+router.get('/', async (ctx: Koa.Context) => {
+  ctx.body = 'Hello world'
+})
+
+app.use(cors())
+app.use(router.routes()).use(router.allowedMethods())
+
+const port = 3000
+app.listen(port, () => {
+  console.log(`server running http://localhost:${port}`)
+})
